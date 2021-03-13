68ba4b15955fc5277e74ea0368a8261342f9e64f
Maik Noungoua <noungouamaik@gmail.com>
2021-03-13 23:57:06 +0100

    working client server, but not accepting consecutive requests

--- client.py
+++ client.py
@@ -1,65 +1,31 @@
 import socket 
-import random 
-import datetime 
-import _thread
-import numpy as np 
-import statistics as stat
-import time 
-host = '192.168.1.154'
+ 
+
+host = '127.0.0.1'
 port = 40000 
 
-print("commands available: \n 1) PUSH\n 2) GET\n 3) NONE")
+CONDITION = [True]
+
+print("commands available: \n 1) parking\n 2) information\n 3) NONE")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 
-# Provide the information about available parking spot////  t
-def cars_motion():
-    global mess, parking_spot_availables, car_id
-                        #### Determination of the car trajectory inside the network ####
-    car_id = 1
-    routes = [1,2,3,4]
-    exit_routes = [1,2,3,4]
-    car_arrival_route = random.choice(routes) # The provenance of the car is chosen randomly
-    exit_routes.remove(car_arrival_route) # the different exit possible for the car 
-    car_exit_route = random.choice(exit_routes) # the car's exit
-    
-
-                        #### Determination of the realtime delay accused ####
-    delay = 10 # expressed in minutes 
-    sigma = 3
-    realtime_delay = int(stat.mean(np.random.normal(delay, sigma, 10))) # the car collects information on the delay of others cars
-     
-    
-                        #### Determination of the parking spot ####
-    spots = [1,2,3,4,5,6,7,8,9]
-    parking_spot_availables = random.choice(spots)
-
-                        #### Final message to be sent ####
-    mess = [car_arrival_route,realtime_delay,car_exit_route,parking_spot_availables,car_id]#this tuple represents the message that will be sent to the access point.
-    return mess 
-
-while True: 
+while CONDITION[0]:
+    c_id = 4 
     command = input("Enter your command: ")
     if command == 'NONE':
-        # Sends exit request to the server
-        s.send(command.encode())
-        break
-    elif command == 'PUSH': 
-        s.send(str(command).encode())
-        infor = cars_motion()
-        s.send(str(infor).encode())
-        break
-    elif command == 'GET':
-        s.send(str(command).encode()) # sends the command GET
-        info = cars_motion()
-        s.send(str(info).encode()) # send the relevant information for car push, stored under the variable mess
-
-        #s.send(command.encode())
-        
+        message = command
+        s.send(message.encode())
+    elif command == 'parking': 
+        message = str(c_id)+'/'+command
+        s.send(message.encode())
+    elif command == 'information':
+        message = str(c_id)+'/'+command
+        s.send(message.encode()) # send the relevant information for car push, stored under the variable mess     
     else: 
     	s.send(command.encode())
-    reply = s.recv(2048)
+    reply = s.recv(1024)
     print(reply.decode('utf-8'))
 
 s.close()
--- server.py
+++ server.py
@@ -1,71 +1,125 @@
+import sys
+sys.path.append(".")
 import socket 
+import _thread
+import statistics as stat
+import numpy as np 
+import random 
+import datetime
+import time 
 
 host = ''
 port = 40000
 
-def GET(information):
-	
-			### AGGREGATION OF THE CAR DELAY ###
-	mean_delay = 10
-	sigma = 3
+CONDITION = [True]
 
-	car_delay = int(car_get[4])
-	lane_delay  = 0
-	if mean_delay < car_delay < mean_delay+sigma:
-		lane_delay  = [car_get[13],car_delay]
+def client_handler(connection): 
+    client_id = 0
+    answer = 0 
+    data = connection.recv(1024)
+    message = data.decode('utf-8')
+    ''' Message is sent  by client side and must contain: 
+          - client_id to reference with register (the list)
+          - action: info or parking; 1 being requesting traffic info from server and 2 requesting parking inside a airport garage'''
+    request = message.split('/')
+    client_id = request[0]
+    action = request[1]
+    if client_id in register: 
+        if action ==  'info': 
+            pass 
+        elif action == 'parking':
+            answer = parking_handler()
+    else: 
+        print("Client not authorised.")
+        connection.close()
+        return
+    connection.sendall(str(answer).encode())
 
-	if mean_delay - sigma < car_delay < mean_delay:
-		lane_delay  = [car_get[13],car_delay] 
 
-	if car_delay < mean_delay - sigma:
-		lane_delay = 'Not applicable'
 
-	if car_delay >  mean_delay + sigma: 
-		lane_delay = 'Not applicable'
-	return lane_delay
 
-def PUSH(message): #This function parsed the information received by cars
-	x = (-len(message))+1
-	message_ = message[x:-1] # message without the brackets, easier to split with delimiter ','
-	print(message_)
-	return x
-	#car_id = 
+'''def GET():
+    
+          ### AGGREGATION OF THE CAR DELAY ###
+    mean_delay = 10
+    sigma = 3
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((host, port)) 
-    s.listen(1) # allows one connection at the time 
-    conn, addr = s.accept()
-    with conn:
-        print("Connected by: ", addr)
-        while True:
-            command = conn.recv(1024)
-            command = command.decode('utf-8')
-            if command == 'PUSH':
-                car_push = conn.recv(1024)
-                car_push = car_push.decode('utf-8')
-                ans = PUSH(car_push)
+    car_delay = int(car_get[4])
+    lane_delay  = 0
+    print(car_get)
+    print(len(car_get))
+    if mean_delay < car_delay < mean_delay+sigma:
+    lane_delay  = [int(car_get[13]),car_delay]
+    
+    elif mean_delay - sigma < car_delay < mean_delay:
+    lane_delay  = [int(car_get[13]),car_delay] 
 
-            if command == 'GET':	
-                car_get = conn.recv(1024)
-                car_get = car_get.decode('utf-8')
-                ans = GET(car_get)
-			
-			
-			
-			#dataMessage = data.split(' ', 1)
-			#command = dataMessage[0]
-			#if command ==  'GET':
-			#	reply = GET()
-				
-			#elif command == 'EXIT':
-			#	print("Connection to client interrupted.")
-			
-			#elif command == 'KILL': 
-			#	print("Our server is shutting down.")
-			#	s.close()
-			#	break 
-			#else: 
-			#	print( "Unknown command.")
-            conn.sendall(str(ans).encode())	
+    elif car_delay < mean_delay - sigma:
+    lane_delay = []
+
+    else: 
+    lane_delay = []
+    print(type(lane_delay))
+    return lane_delay'''
+
+def parking_handler(): #This function parsed the information received by cars
+    available_spot = 104
+    #connection.sendall(str(available_spot.encode())
+    return available_spot
+
+def information_handler(Args):
+    print(Args)
+    print(type(Args))
+    return(Args)
+    
+
+def cars_motion():
+    global mess, parking_spot_availables, car_id
+                        #### Determination of the car trajectory inside the network ####
+    car_id = 1
+    routes = [1,2,3,4]
+    exit_routes = [1,2,3,4]
+    car_arrival_route = random.choice(routes) # The provenance of the car is chosen randomly
+    exit_routes.remove(car_arrival_route) # the different exit possible for the car 
+    car_exit_route = random.choice(exit_routes) # the car's exit
+    
+
+                        #### Determination of the realtime delay accused ####
+    delay = 10 # expressed in minutes 
+    sigma = 3
+    realtime_delay = int(stat.mean(np.random.normal(delay, sigma, 10))) # the car collects information on the delay of others cars
+     
+    
+                        #### Determination of the parking spot ####
+    spots = [1,2,3,4,5,6,7,8,9]
+    
+    parking_spot_availables = random.choice(spots)
+
+                        #### Final message to be sent ####
+    mess = [car_arrival_route,realtime_delay,car_exit_route,parking_spot_availables,car_id]#this tuple represents the message that will be sent to the access point.
+    return mess 
+
+if __name__ == '__main__':
+    global register, information, parking
+    register = ['4','5']
+    #registrer is a list that will implemented somewhere in this code.
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as ServerSideSocket:
+        try: 
+            ServerSideSocket.bind((host, port))
+        except socket.error as e: 
+            ServerSideSocket.close()
+        print('Socket is listenning..')
+        ServerSideSocket.listen(1)
+        while CONDITION[0]: 
+            Client, address = ServerSideSocket.accept()
+            print('Connected to: ' + address[0] + ':' + str(address[1]))
+            _thread.start_new_thread(client_handler, (Client, ))
+
+ 
+    
+
+
+    
 
 
