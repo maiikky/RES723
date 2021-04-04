63d9b184b5f69875338b37a128516bebc07527a7
Maik Noungoua <noungouamaik@gmail.com>
2021-04-04 21:37:51 +0200

    It's working

--- cars2.py
+++ cars2.py
@@ -1,18 +1,20 @@
 import socket 
 import time 
 import _thread 
+import sys, errno
 
 HOST1 = '127.0.0.1'
 PORT1 = 4444
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.connect((HOST1, PORT1))
-command = 'This message notifies a connection'
-s.sendall(bytes(command,'utf-8'))
-message = s.recv(1024).decode('utf-8')
-print(message)
-s.close()
-
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST1, PORT1))
+        command = 'Hello'
+        s.sendall(bytes(command,'utf-8'))
+        message = s.recv(1024).decode('utf-8')
+        print(message)
+        s.close()
+except IOError as e:
+    print("error")
 
 
 
